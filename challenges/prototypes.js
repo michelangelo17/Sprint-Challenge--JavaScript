a5e998a6c5ae2c810b23bad85cd5085ab67e700a
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-18 09:38:31 -0700

    completed classes challenge

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,7 +5,9 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-function CuboidMaker(obj) {
+
+// !!!!!!!!!!!!=========RENAMED CuboidMakerA to prevent errors when running with classes.js=========!!!!!!!!!!!!
+function CuboidMakerA(obj) {
   this.length = obj.length;
   this.width = obj.width;
   this.height = obj.height;
@@ -17,7 +19,7 @@ function CuboidMaker(obj) {
   Formula for cuboid volume: length * width * height
 */
 
-CuboidMaker.prototype.volume = function() {
+CuboidMakerA.prototype.volume = function() {
   return this.length * this.width * this.height;
 }
 
@@ -27,7 +29,7 @@ CuboidMaker.prototype.volume = function() {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surfaceArea = function() {
+CuboidMakerA.prototype.surfaceArea = function() {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
 
@@ -36,7 +38,7 @@ CuboidMaker.prototype.surfaceArea = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = new CuboidMaker({
+const cuboid = new CuboidMakerA({
   length: 4, 
   width: 5,
   height: 5,
