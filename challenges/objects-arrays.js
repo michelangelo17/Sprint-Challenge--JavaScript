d9b592c156c40911b0b71a05df45e7f027cdca49
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-18 08:53:00 -0700

    completed basic array methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,7 +14,7 @@ const tyrannosaurus = {
   weight: '7000kg', 
   length: '12m', 
   period: 'Late Cretaceous',
-  roar: () => "RAWERSRARARWERSARARARRRR!"
+  roar: () => "RAWERSRARARWERSARARARRRR!",
  }
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 const stegosaurus = {
@@ -72,7 +72,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map(graduate => graduate.university);
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -81,11 +81,11 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(graduate => `${graduate.first_name} ${graduate.email}`);
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter(graduate => graduate.university.includes('Uni'));
 console.log(unisWithUni);
 
 
