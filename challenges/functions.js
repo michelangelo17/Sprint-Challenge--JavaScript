ace79c9e9187b4b437089320b7d790db4e8d575e
michelangelo17 <michelangelo.markus@gmail.com>
2019-10-18 08:28:06 -0700

    finished closure question, functions.js is complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -38,7 +38,7 @@ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: When the JS engine executes nestedFunction, it will first look in nestedFunction lexical enviroment for the variable "internal", if it cannot find it, it will then check closure, closure will have both the "internal" variable since even though it's left the myFunction execution context, that context are still in scope for nestedFunction. (As is global.) If you look in the debugger you will see that "internal" is held in closure and "external" is held in the next lexical enviroment up, the script, as well as again in global. So even though external is not called, it is also available to nestedFunction. Since "internal" was called, nestedFunction will pull the value of "internal" from closure and console.log it. If "internal" was nested deeper inside of another lexical exectuion context, it would be inaccessible to the JS engine as that is out of scope. If "internal" was stored further "out" in the global context, it would check each context going further and furth out until it found it in the global. 
 
 
 const external = "I'm outside the function";
@@ -52,4 +52,5 @@ function myFunction() {
   };
   nestedFunction();
 }
+debugger;
 myFunction();
